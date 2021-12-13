46e375322b0ac54c8220c89da0334cec96cdbb7f
Kentaro Saiga <50908462+kentap@users.noreply.github.com>
2021-12-13 22:59:29 +0900

    Update youtube_data.py

--- youtube_data.py
+++ youtube_data.py
@@ -3,7 +3,7 @@
 import pandas as pd
 from apiclient.discovery import build
 
-api_key = 'AIzaSyB8y97s9ppxsMhDwPdRI_TZzSBp262D-oU'
+api_key = 'xxxxxxxxx' # Enter your API key in here.
 api_service_name = 'youtube'
 api_version = 'v3'
 youtube = build(api_service_name, api_version, developerKey=api_key)
